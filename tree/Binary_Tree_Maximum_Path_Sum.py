7f5b37e1e7dddcb3386126193307b05c7c9b8656
mclearning2 <mclearning2@gmail.com>
2019-08-15 13:24:11 +0900

    Update Tree

--- tree/Binary_Tree_Maximum_Path_Sum.py
+++ tree/Binary_Tree_Maximum_Path_Sum.py
@@ -6,12 +6,19 @@
 #         self.right = None
 
 class Solution:
-    def isValidBST(self, root, lower_bound=float('-inf'), upper_bound=float('inf')):
-        if not root:
-            return True
-
-        if root.val <= lower_bound or root.val >= upper_bound:
-            return False
+    def maxPathSum(self, root: TreeNode) -> int:
+        self.max = float('-inf')
         
-        return self.isValidBST(root.left, lower_bound, root.val) and \
-               self.isValidBST(root.right, root.val, upper_bound)
+        def dfs(node):
+            if node is None:
+                return 0
+            
+            l = max(0, dfs(node.left))
+            r = max(0, dfs(node.right))
+            
+            self.max = max(self.max, l + r + node.val)
+            return max(l, r) + node.val
+        
+        dfs(root)
+        
+        return self.max
