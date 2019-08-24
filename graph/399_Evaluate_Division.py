7d3ad277c5aaf63fd9c84e1c52ee881742815186
Mincheol Kim <mclearning2@gmail.com>
2019-08-25 00:59:18 +0900

    Update 399_Evaluate_Division.py

--- graph/399_Evaluate_Division.py
+++ graph/399_Evaluate_Division.py
@@ -1,26 +1,23 @@
+from itertools import permutations
 from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        e = dict()
-        g = defaultdict(list)
-        for (A, B), V in zip(equations, values):
-            g[A].append(B)
-            e[(A, B)] = V
-            e[(B, A)] = 1/V
-        
-        answer = []
-        for q in queries:
-            start, goal = q
-            if start not in g:
-                answer.append(-1.0)
-                continue
+        graph = defaultdict(lambda : defaultdict(lambda: float('inf')))
+        for (f, t), v in zip(equations, values):
+            graph[f][t] = v
+            graph[t][f] = 1/v
+            graph[f][f] = 1.0
+            graph[t][t] = 1.0
             
-        def dfs(node, start, goal, val = 1.0):
-            for next_nodes in g[node]:
-                val *= e[(node, next_nodes)]
-                return dfs(next_nodes, start, goal, val)
-            else: # g[node] is None
-                return val
+        for s, m, e in permutations(graph, 3):
+            if m in graph[s] and e in graph[m]:
+                graph[s][e] = graph[s][m] * graph[m][e]
         
-        return answer
-            
+        l = list()
+        for f, t in queries:
+            if graph[f][t] == float('inf'):
+                l.append(-1.0)
+            else:
+                l.append(graph[f][t])
+    
+        return l
