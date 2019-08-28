c042a2ea574e7bf8f928a06672b5487280cb77d4
Mincheol Kim <mclearning2@gmail.com>
2019-08-29 01:57:36 +0900

    Create 268_Missing_Number.py

---
+++ bit manipulation/268_Missing_Number.py
@@ -0,0 +1,28 @@
+'''
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+'''
+# Solution 1. real sum - num sum
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return len(nums) * (len(nums)+1) // 2 - sum(nums)
+
+# Solution 2. Bit manipulation
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        answer = 0
+        for i, n in enumerate([0] + nums):
+            answer ^= i ^ n
+        return answer
